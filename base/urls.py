97879a21b003cbb4a7cf5ea92151a6a2144841e7
beatriznbt <btrznbt@gmail.com>
2023-10-19 07:34:51 -0300

    atualizações de auth

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import LoginView, HomeView
+from .views import LoginView, HomeView, RegisterView
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
-    path('home/', HomeView.as_view(), name='home')
+    path('home/', HomeView.as_view(), name='home'),
+    path('registrar/', RegisterView.as_view(), name='register'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView 
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
 
 class LoginView(TemplateView):
 
     template_name = 'login.html'
 
+def login(request):
+    return render(request, 'login.html')
 class HomeView(TemplateView):
 
     template_name = 'home.html'
+
+class RegisterView(TemplateView):
+
+    template_name = 'register.html'
+
+    def register(request):
+        if request.method == 'GET':
+            return render(request, 'register.html')
+        else:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            email = request.POST.get('email')
+        
+            return RegisterView.as_view()(request)
+
+
+
